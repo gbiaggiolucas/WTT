29ecb1f03a6f28336c6ecac821231342b9c95251
GustavoBL <gustavobiaggiolucas2007@gmail.com>
2024-06-13 12:19:11 -0300

    atualizado

--- front/script.js
+++ front/script.js
@@ -4,5 +4,17 @@ signup.addEventListener("click", function() {
     window.location.href = "signup.html"
 })
 
-const confirm = document.getElementById("confirm");
+document.getElementById("signin").addEventListener("click", function() {
+    window.location.href = "signin.html"
+})
 
+const email = document.getElementById("email");
+const nome = document.getElementById("nome");
+const senha = document.getElementById("senha");
+const confirmarsenha = document.getElementById("confirmarsenha");
+
+document.getElementById("confirm").addEventListener("click", function() {
+    if (email === " ") {
+        
+    }
+})
