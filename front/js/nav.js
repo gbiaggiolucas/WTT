2a288fd833ec09dccd8fed040ca7aa7387bae5b3
GustavoBL <gustavobiaggiolucas2007@gmail.com>
2024-08-01 11:38:54 -0300

    meta/config

---
+++ front/js/nav.js
@@ -0,0 +1,9 @@
+document.getElementById("metabtn").addEventListener("click", function() {
+    window.location.href = "../html/meta.html"
+})
+document.getElementById("configbtn").addEventListener("click", function() {
+    window.location.href = "../html/config.html"
+})
+document.getElementById("homebtn").addEventListener("click", function() {
+    window.location.href = "../html/wtt.html"
+})
